dfb4042e457104795d47a2941ba0d0dc9414bc88
AaronRey3s <aaronreyesga10@gmail.com>
2024-08-26 18:08:57 -0600

    Actividad 6

---
+++ p29_Calculadora.java
@@ -0,0 +1,45 @@
+// Realiza operaciones matematicas basicas sobre dos numeros
+
+import java.util.Scanner;
+
+public class p29_Calculadora {
+    public static void main(String[] args) {
+        double n1, n2;
+        char op;
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println("\nEfectuando operaciones matematicas basicas sobre dos numeros");
+        System.out.print("Dame numero 1 ? ");
+        n1 = obj.nextFloat();
+        System.out.print("Dame numero 2 ? ");
+        n2 = obj.nextFloat();
+        System.out.print("Operación ( + - * / ^ ) ? ");
+        op = obj.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.printf("n1 + n2 = %.2f", n1 + n2);
+                break;
+            case '-':
+                System.out.printf("n1 - n2 = %.2f", n1 - n2);
+                break;
+            case '*':
+                System.out.printf("n1 * n2 = %.2f", n1 * n2);
+                break;
+            case '/':
+                System.out.printf("n1 / n2 = %.2f", n1 / n2);
+                break;
+            case '^':
+                System.out.printf("n1 / n2 = %.2f", Math.pow(n1, n2));
+                break;
+            default:
+                System.out.printf("\nOperación Inválida ...");
+                break;
+
+        }
+        System.out.println("\n\nProceso terminado ...");
+    }
+
+}
