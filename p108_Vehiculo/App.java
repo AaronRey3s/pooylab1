bb912b5ee8fbf21906d810bc399d74b546b1aeff
AaronRey3s <aaronreyesga10@gmail.com>
2024-10-08 08:50:08 -0600

    Practica 10

---
+++ p108_Vehiculo/App.java
@@ -0,0 +1,29 @@
+package p108_Vehiculo;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.print("\033[H\033[2J");
+        Vehiculo[] vehiculos = new Vehiculo[4];
+        vehiculos[0] = new Compacto("CP0100010122", "Ford", 2023, 125345.22, 4, 2);
+        vehiculos[1] = new Compacto("CP1000145555", "Nissan", 2010, 54500.33, 6, 4);
+        vehiculos[2] = new Camioneta("CA104014544", "Dina", 2008, 234567.15, 450.0, 4);
+        vehiculos[3] = new Camioneta("CA774814599", "Hongyan", 2023, 934577.98, 1200.0, 6);
+
+
+        System.out.println("Datos de los Vehiculos de la flota");
+        for (Vehiculo vehiculo : vehiculos) {
+            System.out.println(vehiculo);
+        }
+
+        
+        double totalPrecios = 0;
+        for (Vehiculo vehiculo : vehiculos) {
+            totalPrecios += vehiculo.getPrecio();
+        }
+
+        System.out.println("Calculando el total de precio de todos los vehículos ...");
+        System.out.println(totalPrecios);
+        System.out.println("La suma de precios es: " + totalPrecios);
+    }
+}
